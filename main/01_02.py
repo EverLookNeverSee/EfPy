ff060e5fd6293beda95520fb09a3f3e59829af99
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-20 20:55:58 +0430

    Adding initial docstring

---
+++ main/01_02.py
@@ -0,0 +1,8 @@
+"""
+    Knowing the difference between bytes and str
+
+        there are two types that represent sequences of character data: bytes and str.
+        Instances of bytes contain raw, unsigned 8-bit values (often displayed in the
+        ASCII encoding). Instances of str contain Unicode code points that represent
+        textual characters from human languages.
+"""
