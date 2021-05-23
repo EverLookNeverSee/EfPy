fa30889c8c7be1211fea7e880fbcac1f0ce2568a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-23 22:30:12 +0430

    Defining and implementing test_to_str function

--- main/tests/test_01_02.py
+++ main/tests/test_01_02.py
@@ -1,2 +1,8 @@
 import pytest
 from ..chapter_01_item_02 import to_str, to_bytes
+
+
+def test_to_str() -> None:
+    assert to_str(b"foo") == "foo"
+    assert to_str(b"hello") == "hello"
+    assert to_str(b"Python") == "Python"
