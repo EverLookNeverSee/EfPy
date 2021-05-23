1155bf341fc6e949870b8540508b923b0028f1f3
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-23 22:32:01 +0430

    Defining and implementing test_to_bytes function

--- main/tests/test_01_02.py
+++ main/tests/test_01_02.py
@@ -6,3 +6,9 @@ def test_to_str() -> None:
     assert to_str(b"foo") == "foo"
     assert to_str(b"hello") == "hello"
     assert to_str(b"Python") == "Python"
+
+
+def test_to_bytes() -> None:
+    assert to_bytes("foo") == b"foo"
+    assert to_bytes("hello") == b"hello"
+    assert to_bytes("Python") == b"Python"
