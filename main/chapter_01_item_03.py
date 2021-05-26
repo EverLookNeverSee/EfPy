33d8137ceb798437b0b655281c2425278c1210c0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-26 22:46:02 +0430

    Converting binary and hexadecimal values to integer strings

--- main/chapter_01_item_03.py
+++ main/chapter_01_item_03.py
@@ -5,3 +5,7 @@
 # Defining binary and hexadecimal variables
 binary = 0b10111011
 hexadecimal = 0xc5f
+
+if __name__ == '__main__':
+    # Converting binary and hexadecimal values to integer strings
+    print("Binary is %d, hexadecimal is %d" % (binary, hexadecimal))
